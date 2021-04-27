3fb7b1781dfce0f31804b00201886c1ec4113e5f
BButner <butner.beau@gmail.com>
2021-04-27 11:29:53 -0500

    Minor icon tweaks

--- src/lib/icon.ts
+++ src/lib/icon.ts
@@ -105,7 +105,7 @@ export class IconHandler {
   private getSVGString = (iconStyle: IconStyleType): string => {
     const color: string = getConfigurationValue(ConfigurationOption.iconColor)! // Force string because it will return the custom value, OR the default value
 
-    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" transform-origin="0 0 12 12" ` + (iconStyle === IconStyleType.solid ?
+    return `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22 22" ` + (iconStyle === IconStyleType.solid ?
       `fill="${color}"`.replace('#', '%23') : `fill="none" stroke="${color}"`.replace('#', '%23'))
       + '>'
   }
