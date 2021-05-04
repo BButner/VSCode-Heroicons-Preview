3e562d515df5e2194e30ec108a49e9d1e4e67dbb
BButner <butner.beau@gmail.com>
2021-05-04 11:14:57 -0500

    Cleanup and adds icon

--- src/extension.ts
+++ src/extension.ts
@@ -3,25 +3,9 @@ import { Events } from './lib/events';
 import { detectHeroIcons } from './lib/file'
 import { IconHandler } from './lib/icon';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Heroicons Preview is now active.');
 
-	// initEventHandlers()
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// let disposable = vscode.commands.registerCommand('heroicons-preview.hipTest', () => {
-	// 	svg()
-	// });
-
-	// context.subscriptions.push(disposable);
-
-	// if (vscode.window.activeTextEditor) {
-	// 	svg()
-	// }
 	detectHeroIcons()
 		.then(location => {
 			if (location) {
@@ -33,5 +17,4 @@ export function activate(context: vscode.ExtensionContext) {
 		})
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
